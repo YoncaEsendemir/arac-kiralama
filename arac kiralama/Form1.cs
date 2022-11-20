183de1b1a3888bb8157fa0accebefa58af6ebc22
yonca esendemır <esendemiryonca@gmail.com>
2022-11-20 19:33:18 +0300

    başlangıç

--- arac kiralama/Form1.cs
+++ arac kiralama/Form1.cs
@@ -6,5 +6,37 @@ namespace arac_kiralama
         {
             InitializeComponent();
         }
+
+        private void buttonCikis4_Click(object sender, EventArgs e)
+        {
+            // thisClose;
+            DialogResult result1 = MessageBox.Show("Cikmak istdiginize emin miziniz?","",MessageBoxButtons.YesNo);
+            if(result1 == DialogResult.Yes)
+            {
+                Application.Exit();
+             }
+            else
+            {
+
+            }
+        }
+
+        private void buttonKayit1_Click(object sender, EventArgs e)
+        {
+            if(müsteriKayitPanel.Visible==true)
+            {
+                müsteriKayitPanel.Visible = false;
+            }
+            else
+            {
+                müsteriKayitPanel.Visible = true;
+            }
+
+
+
+
+        }
+
+
     }
 }
